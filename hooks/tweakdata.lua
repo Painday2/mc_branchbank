5a4fa7704be11f6ba5fa4ad5deb20c17c6252f2e
Mako <masterjon902@gmail.com>
2020-12-09 16:03:58 -0600

    haha font go (cube)

--- hooks/tweakdata.lua
+++ hooks/tweakdata.lua
@@ -1,3 +1,17 @@
 	tweak_data.pickups.tripwire_key = {
 		unit = Idstring("units/pd2_mod_craft/props/tripwire_key")
 	}
+
+	tweak_data.menu.default_font = "fonts/minecraft"
+	tweak_data.menu.default_font_no_outline = "fonts/minecraft"
+	tweak_data.menu.medium_font = "fonts/minecraft"
+	tweak_data.menu.medium_font_no_outline = "fonts/minecraft"
+	tweak_data.menu.small_font = "fonts/minecraft"
+	tweak_data.menu.pd2_massive_font = "fonts/minecraft"
+	tweak_data.menu.pd2_large_font = "fonts/minecraft"
+	tweak_data.menu.pd2_medium_font = "fonts/minecraft"
+	tweak_data.menu.pd2_small_font = "fonts/minecraft"
+	tweak_data.menu.pd2_tiny_font = "fonts/minecraft"
+	tweak_data.hud.medium_font = "fonts/minecraft"
+	tweak_data.hud.medium_font_noshadow = "fonts/minecraft"
+	tweak_data.hud.small_font = "fonts/minecraft"
