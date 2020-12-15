06100e35a56b5df335dbc48ae3101502791dfe86
Mako <masterjon902@gmail.com>
2020-12-14 22:32:13 -0600

    i hate this video game
    fucking
    start on interaction ext for items
    why do numbers not fucking work for items after stick
    
    i hate this vuideo game

--- classes/MCcrafting.lua
+++ classes/MCcrafting.lua
@@ -1,15 +1,23 @@
 MCcrafting = MCcrafting or class()
 
-log("[EEEEEEEEEEEEEEEEEEEEE]")
+function MCcrafting:init()
 
-local recipes = {
-    --Item name (needs to be same as in equipmenttweakdata) = "123456789" (Recipe, 1 is top left, 9 is bottom right)
-    stick = "EEEWEEWEE",
-    woodpick = "WWWESEESE"
-}
+    log("[MCcrafting] Init")
 
+    self._items = {
+        stick = 0,
+        cobblestone = 0,
+    }
+    self._recipes = {
+        --Item name (needs to be same as in equipmenttweakdata) = "123456789" (Recipe, 1 is top left, 9 is bottom right)
+        stick = "EEEWEEWEE",
+        woodpick = "WWWESEESE"
+    }
+end
 local inGrid = {"E","E","E","W","E","E","W","E","E"}
 local grid = table.concat(inGrid)
+
+MCcrafting:init()
 
 function MCcrafting:checkRecipe()
     for i,v in pairs(recipes) do
@@ -34,4 +42,65 @@ end
 function MCcrafting:aaaaa()
     local item = MCcrafting:checkRecipe()
     MCcrafting:TakeItem(item)
+end
+
+
+
+mcItemInteractionExt = mcItemInteractionExt or class(UseInteractionExt)
+
+function mcItemInteractionExt:_interact_blocked(player)
+	local can_pickup, has_max_quantity = managers.player:can_pickup_equipment(self._special_equipment)
+
+	return not can_pickup, false, has_max_quantity and "max_special_equipment" or nil
+end
+
+function mcItemInteractionExt:interact(player)
+	if not alive(self._unit) then
+		return
+	end
+
+	mcItemInteractionExt.super.super.interact(self, player)
+
+	if self._remove_on_interact then
+		self:remove_interact()
+		self:set_active(false)
+	end
+
+	managers.network:session():send_to_peers_synched("sync_interacted", self._unit, -2, self.tweak_data, 1)
+	self:sync_interacted(nil, player)
+	self:apply_item_pickup()
+
+	return true
+end
+
+function mcItemInteractionExt:sync_interacted(peer, player, status, skip_alive_check)
+	player = player or peer:unit()
+
+	if self._unit:damage():has_sequence("load") then
+		self._unit:damage():run_sequence_simple("load")
+	end
+
+	if self._global_event and Network:is_server() then
+		managers.mission:call_global_event(self._global_event, player)
+	end
+
+	if self._remove_on_interact then
+		self._unit:set_slot(0)
+	end
+end
+
+function mcItemInteractionExt:set_give_special_equipment(special_equipment)
+	self._special_equipment = special_equipment
+end
+
+function mcItemInteractionExt:apply_item_pickup()
+    if self._special_equipment then
+        PrintTable(MCcrafting._items)
+        MCcrafting._items[self._special_equipment] = MCcrafting._items[self._special_equipment] + self._amount
+        PrintTable(MCcrafting._items)
+	end
+end
+
+function mcItemInteractionExt:can_remove_item()
+	return self._remove_on_interact
 end
--- classes/MCcraftingMenu.lua
+++ classes/MCcraftingMenu.lua
@@ -7,7 +7,7 @@ local CraftMenu = MCcrafting.Menu
 function CraftMenu:init()
     self._menu = MenuUI:new({
         name = "CraftMenu",
-		layer = 2500,
+		layer = BaseLayer,
         use_default_close_key = true,
         disable_player_controls = true,
 		background_color = Color(0,0,0, 0.5),
@@ -18,6 +18,7 @@ function CraftMenu:init()
     self:_init_tweakdata()
     self:_init_bg()
     self:_init_craft_gui()
+    self:SetInventory()
 end
 
 function CraftMenu:_init_tweakdata()
@@ -70,8 +71,8 @@ function CraftMenu:_init_craft_gui()
         scrollbar = false,
         align_method = "grid"
     })
-
-    local CraftingSlot_1 = self.CraftingPanels:ImageButton({
+    self.CraftingSlot = {}
+    self.CraftingSlot[1] = self.CraftingPanels:ImageButton({
         name = "CraftingSlot_1",
         texture = "guis/textures/pd2/none_icon",
         w = 26,
@@ -81,7 +82,7 @@ function CraftMenu:_init_craft_gui()
         end
     })
 
-    local CraftingSlot_2 = self.CraftingPanels:ImageButton({
+    self.CraftingSlot[2] = self.CraftingPanels:ImageButton({
         name = "CraftingSlot_2",
         texture = "guis/textures/pd2/none_icon",
         position = function(item)
@@ -94,7 +95,7 @@ function CraftMenu:_init_craft_gui()
         h = 26,
     })
 
-    local CraftingSlot_3 = self.CraftingPanels:ImageButton({
+    self.CraftingSlot[3] = self.CraftingPanels:ImageButton({
         name = "CraftingSlot_3",
         texture = "guis/textures/pd2/none_icon",
         position = function(item)
@@ -117,7 +118,7 @@ function CraftMenu:_init_craft_gui()
         align_method = "grid"
     })
 
-    local CraftingSlot_4 = self.CraftingPanel_2:ImageButton({
+    self.CraftingSlot[4] = self.CraftingPanel_2:ImageButton({
         name = "CraftingSlot_4",
         texture = "guis/textures/pd2/none_icon",
         w = 26,
@@ -127,7 +128,7 @@ function CraftMenu:_init_craft_gui()
         end
     })
 
-    local CraftingSlot_5 = self.CraftingPanel_2:ImageButton({
+    self.CraftingSlot[5] = self.CraftingPanel_2:ImageButton({
         name = "CraftingSlot_5",
         texture = "guis/textures/pd2/none_icon",
         position = function(item)
@@ -140,7 +141,7 @@ function CraftMenu:_init_craft_gui()
         h = 26,
     })
 
-    local CraftingSlot_6 = self.CraftingPanel_2:ImageButton({
+    self.CraftingSlot[6] = self.CraftingPanel_2:ImageButton({
         name = "CraftingSlot_6",
         texture = "guis/textures/pd2/none_icon",
         position = function(item)
@@ -163,7 +164,7 @@ function CraftMenu:_init_craft_gui()
         align_method = "grid"
     })
 
-    local CraftingSlot_7 = self.CraftingPanel_3:ImageButton({
+    self.CraftingSlot[7] = self.CraftingPanel_3:ImageButton({
         name = "CraftingSlot_7",
         texture = "guis/textures/pd2/none_icon",
         w = 26,
@@ -173,7 +174,7 @@ function CraftMenu:_init_craft_gui()
         end
     })
 
-    local CraftingSlot_8 = self.CraftingPanel_3:ImageButton({
+    self.CraftingSlot[8] = self.CraftingPanel_3:ImageButton({
         name = "CraftingSlot_8",
         texture = "guis/textures/pd2/none_icon",
         position = function(item)
@@ -186,7 +187,7 @@ function CraftMenu:_init_craft_gui()
         h = 26,
     })
 
-    local CraftingSlot_9 = self.CraftingPanel_3:ImageButton({
+    self.CraftingSlot[9] = self.CraftingPanel_3:ImageButton({
         name = "CraftingSlot_9",
         texture = "guis/textures/pd2/none_icon",
         position = function(item)
@@ -198,7 +199,166 @@ function CraftMenu:_init_craft_gui()
         w = 26,
         h = 26,
     })
+
+    self.InventoryPanel = self.CraftGUIMenu:Menu({
+        name = "InventoryPanel",
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 15)
+            item:Panel():set_y(item:ParentPanel():y() + 275)
+        end,
+        layer = BaseLayer + 1,
+        scrollbar = false,
+        align_method = "grid"
+    })
+    self.InventorySlot = {}
+    self.InventorySlotNumbers = {}
+    self.InventorySlot["stick"] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_stick",
+        texture = "guis/textures/pd2/none_icon",
+        w = 26,
+        h = 26,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 1 was pressed!")
+        end
+    })
+
+    self.InventorySlotNumbers["stick"] = self.InventorySlot["stick"]:Divider({
+        name = "sticknumbers",
+        text = self.InventorySlot["stick"].amount,
+        font = Font,
+        size = 32,
+        font_size = 18,
+        text_vertical = "center",
+        text_align = "center",
+        position = function(item)
+            item:Panel():set_bottom(item:ParentPanel():bottom() + 2)
+            item:Panel():set_left(item:ParentPanel():left())
+            item:Panel():set_right(item:ParentPanel():right() - 2)
+        end
+    })
+
+    self.InventorySlot["cobblestone"] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_cobblestone",
+        texture = "guis/textures/pd2/none_icon",
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 40)
+        end,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 2 was pressed!")
+        end,
+        w = 26,
+        h = 26,
+    })
+
+    self.InventorySlot[3] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_3",
+        texture = "guis/textures/pd2/none_icon",
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 72)
+        end,
+        on_callback = function(item)
+            log("inv 3 was pressed!")
+        end,
+        amount = 0,
+        w = 26,
+        h = 26,
+    })
+
+    self.InventorySlot[4] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_4",
+        texture = "guis/textures/pd2/none_icon",
+        w = 26,
+        h = 26,
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 106)
+        end,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 4 was pressed!")
+        end
+    })
+
+    self.InventorySlot[5] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_5",
+        texture = "guis/textures/pd2/none_icon",
+        w = 26,
+        h = 26,
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 138)
+        end,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 5 was pressed!")
+        end
+    })
+
+    self.InventorySlot[6] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_6",
+        texture = "guis/textures/pd2/none_icon",
+        w = 26,
+        h = 26,
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 172)
+        end,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 6 was pressed!")
+        end
+    })
+
+    self.InventorySlot[7] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_7",
+        texture = "guis/textures/pd2/none_icon",
+        w = 26,
+        h = 26,
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 204)
+        end,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 7 was pressed!")
+        end
+    })
+
+    self.InventorySlot[8] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_8",
+        texture = "guis/textures/pd2/none_icon",
+        w = 26,
+        h = 26,
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 236)
+        end,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 8 was pressed!")
+        end
+    })
+
+    self.InventorySlot[9] = self.InventoryPanel:ImageButton({
+        name = "InventorySlot_9",
+        texture = "guis/textures/pd2/none_icon",
+        w = 26,
+        h = 26,
+        position = function(item)
+            item:Panel():set_x(item:ParentPanel():x() + 268)
+        end,
+        amount = 0,
+        on_callback = function(item)
+            log("inv 9 was pressed!")
+        end
+    })
 end
 function CraftMenu:toggle()
     self._menu:SetEnabled(true)
+end
+--MCcrafting.Menu:SetInventory()
+function CraftMenu:SetInventory()
+    for i,v in pairs(MCcrafting._items) do
+        self.InventorySlot[i].amount = self.InventorySlot[i].amount + v
+    end
+    for i, v in pairs(self.InventorySlotNumbers) do
+        self.InventorySlotNumbers[i]:SetText(self.InventorySlot[i].amount)
+    end
 end
