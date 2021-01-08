d9412c39d10e70e046129ef5ace516331722caf1
Mako <masterjon902@gmail.com>
2021-01-08 14:08:10 -0600

    icons, tidying of hooks, fixed crashing if achievements didn't exist

--- hooks/outofmap/blackmarketmanager.lua
+++ hooks/outofmap/blackmarketmanager.lua
@@ -1,6 +1,8 @@
---not gonna lie, stole the base of this from fed.net 
+--not gonna lie, stole the base of this from fed.net
 
 local Locked_Bool = {true, true}
+local materials = {"stone","iron","gold","diamond","netherite"}
+local already_setup = SystemFS:exists(SavePath .. "CustomAchievements/" ..tostring(Steam:userid()).."/mc_branchbank_ach")
 CustomAchievementPackage:init("mc_branchbank_ach")
 
 Hooks:PostHook(BlackMarketManager, "_load_done", "mc_bb_load_done", function(bb, ...)
@@ -18,38 +20,19 @@ Hooks:PostHook(BlackMarketManager, "_setup", "mc_bb_setup", function(self, ...)
 end)
 
 function LockMCWeapons()
-		
-	for melee_id, melee_data in pairs(Global.blackmarket_manager.melee_weapons) do
-		local _md = tweak_data.blackmarket.melee_weapons[melee_id] or nil
-        if _md and _md.dlc == 'shovelforge_stone' then
-            local Achievement = CustomAchievementPackage:Achievement("stone")
-            if not Achievement:IsUnlocked("stone") then
-                Global.blackmarket_manager.melee_weapons[melee_id].unlocked = false
+    for melee_id, melee_data in pairs(Global.blackmarket_manager.melee_weapons) do
+        local _md = tweak_data.blackmarket.melee_weapons[melee_id] or nil
+        for key, value in pairs(materials) do
+            if _md and _md.dlc == 'shovelforge_'..value then
+                if already_setup then
+                    local Achievement = CustomAchievementPackage:Achievement(value)
+                    if not Achievement:IsUnlocked(value) then
+                        Global.blackmarket_manager.melee_weapons[melee_id].unlocked = false
+                    end
+                else
+                    Global.blackmarket_manager.melee_weapons[melee_id].unlocked = false
+                end
             end
         end
-        if _md and _md.dlc == 'shovelforge_iron' then
-            local Achievement = CustomAchievementPackage:Achievement("iron")
-            if not Achievement:IsUnlocked("iron") then
-                Global.blackmarket_manager.melee_weapons[melee_id].unlocked = false
-            end
-        end
-        if _md and _md.dlc == 'shovelforge_gold' then
-            local Achievement = CustomAchievementPackage:Achievement("gold")
-            if not Achievement:IsUnlocked("gold") then
-                Global.blackmarket_manager.melee_weapons[melee_id].unlocked = false
-            end
-        end
-        if _md and _md.dlc == 'shovelforge_diamond' then
-            local Achievement = CustomAchievementPackage:Achievement("diamond")
-            if not Achievement:IsUnlocked("diamond") then
-                Global.blackmarket_manager.melee_weapons[melee_id].unlocked = false
-            end
-        end
-        if _md and _md.dlc == 'shovelforge_netherite' then
-            local Achievement = CustomAchievementPackage:Achievement("netherite")
-            if not Achievement:IsUnlocked("netherite") then
-                Global.blackmarket_manager.melee_weapons[melee_id].unlocked = false
-            end
-        end
-	end
+    end
 end
--- hooks/outofmap/meleeweaponstweakdata.lua
+++ hooks/outofmap/meleeweaponstweakdata.lua
@@ -1,38 +1,20 @@
 Hooks:PostHook(BlackMarketTweakData, "_init_melee_weapons", "mc_bb_init_melee_weapons", function(self, tweak_data)
 	-- this is way too much, probably make a seperate mod that'll actually have ALL of them that works with this stuff
 	-- or just say screw it and add all them anyways lmao what could go wrong :vvvbap:
-	local wood_melees = {'mcwshovel','mcwsword','mcwpickaxe','mcwaxe','mcwhoe'}
-	local stone_melees = {'mcsshovel','mcssword','mcspickaxe','mcsaxe','mcshoe'}
-	local iron_melees = {'mcishovel','mcisword','mcipickaxe','mciaxe','mcihoe'}
-	local gold_melees = {'mcgshovel','mcgsword','mcgpickaxe','mcgaxe','mcghoe'}
-	local diamond_melees = {'mcdshovel','mcdsword','mcdpickaxe','mcdaxe','mcdhoe'}
-	local netherite_melees = {'mcnshovel','mcnsword','mcnpickaxe','mcnaxe','mcnhoe'}
-
+	local materials = {
+		wood = {'mcwshovel','mcwsword','mcwpickaxe','mcwaxe','mcwhoe'},
+		stone = {'mcsshovel','mcssword','mcspickaxe','mcsaxe','mcshoe'},
+		iron = {'mcishovel','mcisword','mcipickaxe','mciaxe','mcihoe'},
+		gold = {'mcgshovel','mcgsword','mcgpickaxe','mcgaxe','mcghoe'},
+		diamond = {'mcdshovel','mcdsword','mcdpickaxe','mcdaxe','mcdhoe'},
+		netherite = {'mcnshovel','mcnsword','mcnpickaxe','mcnaxe','mcnhoe'}
+	}
 	for weapon_id, weapon_data in pairs(self.melee_weapons) do
-		if wood_melees[weapon_id] then
-			self.melee_weapons[weapon_id].dlc = "shovelforge_wood"
-			self.melee_weapons[weapon_id].locks = {dlc = "shovelforge_wood"}
-		end
-		if stone_melees[weapon_id] then
-			self.melee_weapons[weapon_id].dlc = "shovelforge_stone"
-			self.melee_weapons[weapon_id].locks = {dlc = "shovelforge_stone"}
-		end
-		if iron_melees[weapon_id] then
-			self.melee_weapons[weapon_id].dlc = "shovelforge_iron"
-			self.melee_weapons[weapon_id].locks = {dlc = "shovelforge_iron"}
-		end
-		if gold_melees[weapon_id] then
-			self.melee_weapons[weapon_id].dlc = "shovelforge_gold"
-			self.melee_weapons[weapon_id].locks = {dlc = "shovelforge_gold"}
-		end
-		if diamond_melees[weapon_id] then
-			self.melee_weapons[weapon_id].dlc = "shovelforge_diamond"
-			self.melee_weapons[weapon_id].locks = {dlc = "shovelforge_diamond"}
-		end
-		if netherite_melees[weapon_id] then
-			self.melee_weapons[weapon_id].dlc = "shovelforge_netherite"
-			self.melee_weapons[weapon_id].locks = {dlc = "shovelforge_netherite"}
+		for k, v in pairs(materials) do
+			if k[weapon_id] then
+				self.melee_weapons[weapon_id].dlc = "shovelforge_".. k
+				self.melee_weapons[weapon_id].locks = {dlc = "shovelforge_"..k}
+			end
 		end
 	end
-
 end)
