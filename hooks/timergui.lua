9b4444511d1c196b2a3aabdb21a7fc785f5135f9
Mako <masterjon902@gmail.com>
2022-03-14 01:03:14 -0500

    Drill sounds - Update MCLib
    Active sound needs to be lengthened

--- hooks/timergui.lua
+++ hooks/timergui.lua
@@ -2,3 +2,9 @@ Hooks:PostHook(TimerGui, "setup", "DrillScreenFix", function(self)
 	--Lowers drill screen brightness
 	self._gui_script.panel:set_alpha(0.6)
 end)
+--Forces the drill sound to stop looping, prehook to allow the next sound to play
+Hooks:PreHook(TimerGui, "post_event", "mc_bb_force_stop_drill_sound", function(self, event)
+	if event and event == self._jam_event or event == self._resume_event then
+		self._unit:sound_source():post_event("mc_drill_stop_all", nil, nil, unpack({}))
+	end
+end)
