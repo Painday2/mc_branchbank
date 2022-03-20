668c037e7d99486aef40fd1f3cfda50a39141075
Mako <masterjon902@gmail.com>
2022-03-20 14:33:06 -0500

    Moved crafting to MCLib

--- classes/MCcraftinginteraction.lua
+++
@@ -1,5 +0,0 @@
-return function ()
-    MCCrafting.Menu:init()
-    MCCrafting.Menu:toggle()
-    return true
-end
--- hooks/setup.lua
+++
@@ -1,3 +0,0 @@
-dofile(mc_branchbank.ModPath .. "classes/MCCrafting.lua")
-dofile(mc_branchbank.ModPath .. "classes/MCCraftingTweakData.lua")
-dofile(mc_branchbank.ModPath .. "classes/MCCraftingMenu.lua")
