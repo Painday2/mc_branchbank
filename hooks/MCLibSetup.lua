91724c772fc62183d5771142b7fc9283e3f63a14
Mako <masterjon902@gmail.com>
2022-03-20 19:49:05 -0500

    Create MCLibSetup.lua
    i might get rid of this later

---
+++ hooks/MCLibSetup.lua
@@ -0,0 +1,10 @@
+return function()
+    --This table should contain any craftable items that should turn into equipment
+    --you will still need to add the equipment to the tweakdata yourself
+    --This ID should be the same across tweak_data.items.id and equipmenttweakdata
+    --EXAMPLE: mccrafting.tweak_data.items.lever and tweak_data.equipments.specials.lever
+    MCCrafting.tweak_data.equipment = {
+        "lever",
+        "mc_drill",
+    }
+end
